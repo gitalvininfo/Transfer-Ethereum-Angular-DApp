5a544c9a47376e2a972d51e68897e970fbc5e7a0
Alvin <yanson.alvin.info@gmail.com>
2021-09-06 17:50:15 +0800

    smart contract as middleman lets play some dota2

--- Frontend/src/app/services/contract.service.ts
+++ Frontend/src/app/services/contract.service.ts
@@ -8,6 +8,7 @@ const tokenAbi = require('../../../../Contracts/build/contracts/Payment.json');
 declare let window: any;
 const contract = require('@truffle/contract');
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,53 +20,58 @@ export class ContractService {
   private accounts: string[];
   public accountsObservable = new Subject<string[]>();
   public success: boolean;
+
   constructor(private snackbar: MatSnackBar) {
+    if(window.ethereum === undefined) {
+      alert('Non-Ethereum browser detected. Install MetaMask');
+    }
     if (typeof window.web3 === 'undefined' || (typeof window.ethereum !== 'undefined')) {
       this.web3Provider = window.ethereum || window.web3;
       window.web3 = new Web3(this.web3Provider);
-    } else {
-      this.web3Provider = new Web3(new Web3.providers.HttpProvider('http://134.209.56.46:1919'));
+    }
+    else {
+      this.web3Provider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
       // Material use custom endpoint please change with your key.
       // if you are using linix or ganche cli maybe the port is  http://localhost:8545
       //   Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
       //   this.web3Provider = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/Private_key'));
       // Change with your credentials as the test network and private key in infura.io
-       }
-     }
+    }
 
+    console.warn(this.web3Provider)
+  }
 
-
-
+  
 
   seeAccountInfo() {
     return new Promise((resolve, reject) => {
       window.web3.eth.getCoinbase((err, account) => {
-          if (account === true) {
-            console.log('dondt work' + account);
-            return reject({name: 'account'});
-          } else {
-            window.web3.eth.getBalance(account, (error, balance) => {
-              if (balance !== null) {
-                return resolve({
-                  originAccount: account,
-                  balance: (window.web3.utils.fromWei(balance, 'ether'))
-                });
-              } else {
-                return reject({name: 'balance'});
-              }
-            });
-          }
+        console.log(account);
+        if (account === true) {
+          return reject({ name: 'account' });
+        } else {
+          window.web3.eth.getBalance(account, (error, balance) => {
+            if (balance !== null) {
+              return resolve({
+                originAccount: account,
+                balance: (window.web3.utils.fromWei(balance, 'ether'))
+              });
+            } else {
+              return reject({ name: 'balance' });
+            }
+          });
+        }
       });
     });
   }
 
-   refreshAccounts() {
+  refreshAccounts() {
 
     window.web3.eth.getAccounts((err, accs) => {
       console.log('Refreshing accounts');
       if (err === true) {
         console.warn('There was an error fetching your accounts.');
-        console.log(err , accs);
+        console.log(err, accs);
         return;
       }
 
@@ -83,43 +89,48 @@ export class ContractService {
       }
 
       console.log('ready');
-   });
+    });
   }
 
   trasnferEther(originAccount, destinyAccount, amount) {
-    const that = this;
+    const that = this; 
 
     return new Promise((resolve, reject) => {
       const paymentContract = contract(tokenAbi);
       paymentContract.setProvider(that.web3Provider);
 
       paymentContract.deployed().then((instance) => {
-          return instance.nuevaTransaccion(
-            destinyAccount,
-            {
-              from: originAccount,
-              value: window.web3.utils.toWei(amount, 'ether')
-            });
-        }).then((status) => {
-          if (status) {
-            return resolve({status: true});
-          }
-        }).catch((error) => {
-          console.log(error);
+        console.warn({
+          destiny: destinyAccount,
+          from: originAccount
+        })
+        return instance.transfer(
+          destinyAccount,
+          {
+            from: originAccount,
+            value: window.web3.utils.toWei(amount, 'ether')
+          });
+      }).then((status) => {
+        if (status) {
+          return resolve({ status: true });
+        }
+      }).catch((error) => {
+        console.log(error);
 
-          return reject('Error transfering Ether');
-        });
+        return reject('Error transfering Ether');
+      });
     });
   }
 
 
 
   failure(message: string) {
-     this.snackbar.open(message, '' , {   duration: 3000
-     });
+    this.snackbar.open(message, '', {
+      duration: 3000
+    });
   }
 
   succes() {
-   this.snackbar.open('Transaction complete successfuly', '' , { duration: 300 } );
+    this.snackbar.open('Transaction complete successfuly', '', { duration: 300 });
   }
 }
--- Frontend/src/app/transaction/transaction.component.ts
+++ Frontend/src/app/transaction/transaction.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 // Services
 import { ContractService } from '../services/contract.service';
-import { UploadService} from '../services/upload.service';
 type TransactionField = 'sendaddress' | 'amount';
-type FormErrors = {[u in TransactionField]: string};
+type FormErrors = { [u in TransactionField]: string };
+
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
@@ -19,8 +19,6 @@ export class TransactionComponent implements OnInit {
   success: boolean;
   compatible: boolean;
   transactionDone: boolean;
-  error;
-  uploadResponse = { status: '', message: '', filePath: '' };
   transactionForm: FormGroup;
   form: FormGroup;
   formErrors: FormErrors = {
@@ -28,21 +26,22 @@ export class TransactionComponent implements OnInit {
     amount: '',
   };
   validationMessages = {
-   sendaddress: {
-   required: 'The send address is required ',
-   pattern: 'that´s no looks like a valid address',
-   minlength: 'a address must have much than 40 characters',
+    sendaddress: {
+      required: 'The send address is required ',
+      pattern: 'that´s no looks like a valid address',
+      minlength: 'a address must have much than 40 characters',
 
-   },
-   amount: {
-     required: 'Need a amount to sent to address',
-     pattern: 'Only support numbers',
-   },
+    },
+    amount: {
+      required: 'Need a amount to sent to address',
+      pattern: 'Only support numbers',
+    },
   };
 
-// tslint:disable-next-line: no-shadowed-variable
-  constructor(private frb: FormBuilder, private contract: ContractService, private upload: UploadService, private snackbar: MatSnackBar) {
-   contract.seeAccountInfo().then((value: any) => {
+  // tslint:disable-next-line: no-shadowed-variable
+  constructor(private frb: FormBuilder, private contract: ContractService, private snackbar: MatSnackBar) {
+    contract.seeAccountInfo().then((value: any) => {
+      console.warn(value)
       this.direction = value.originAccount;
       this.balance = value.balance;
     }).catch((error: any) => {
@@ -53,22 +52,19 @@ export class TransactionComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm();
-    this.form = this.frb.group({
-      file: ['']
-    });
   }
 
   buildForm() {
     this.transactionForm = this.frb.group({
       sendaddress: ['', [
-          Validators.required,
-          Validators.minLength(42),
-        ]
+        Validators.required,
+        Validators.minLength(42),
+      ]
       ],
-      amount : ['', [
-          Validators.required,
-          Validators.pattern(/^[+-]?\d+(\.\d+)?$/),
-        ]
+      amount: ['', [
+        Validators.required,
+        Validators.pattern(/^[+-]?\d+(\.\d+)?$/),
+      ]
       ],
     });
     this.transactionForm.valueChanges.subscribe((data) => this.onValueChanged(data));
@@ -85,27 +81,12 @@ export class TransactionComponent implements OnInit {
 
     this.contract.trasnferEther(this.direction, this.address, this.amount).then((r) => {
       this.contract.succes();
-// tslint:disable-next-line: no-shadowed-variable
+      // tslint:disable-next-line: no-shadowed-variable
     }).catch((e) => {
       this.contract.failure('Transaction failed');
     });
   }
-  onFileChange(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.form.get('file').setValue(file);
-    }
-  }
 
-  onSubmit() {
-    const formData = new FormData();
-    formData.append('file', this.form.get('file').value);
-
-    this.upload.upload(formData , 1).subscribe(
-      (res) => this.uploadResponse = res,
-      (err) => this.error = err,
-    );
-  }
   onValueChanged(data?: any) {
     if (!this.transactionForm) { return; }
     const form = this.transactionForm;
@@ -117,8 +98,8 @@ export class TransactionComponent implements OnInit {
           const messages = this.validationMessages[field];
           if (control.errors) {
             for (const key in control.errors) {
-              if (Object.prototype.hasOwnProperty.call(control.errors, key) ) {
-                this.formErrors[field] += `${(messages as {[key: string]: string})[key]} `;
+              if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
+                this.formErrors[field] += `${(messages as { [key: string]: string })[key]} `;
               }
             }
           }
